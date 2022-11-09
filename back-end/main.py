53b3051544651ad6ffca01a3b5f4924460bbc148
adamd <adamdoyle630@gmail.com>
2022-11-09 10:47:59 -0500

    Update main.py

--- back-end/main.py
+++ back-end/main.py
@@ -2,6 +2,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/api/health")
 def read_root():
     return "Hello World!"
